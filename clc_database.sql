26bf1d28409472bb1c5dde93e4157c71e6933b78
hannah.morrison <hannah.morrison@centre.edu>
2025-10-22 01:59:37 +0000

    Added in all tables

--- clc_database.sql
+++ clc_database.sql
@@ -10,27 +10,23 @@ CREATE TABLE slots
     building_name     VARCHAR(7),
     subject_code      INT,
     class_number      INT,
-    tutor_id          INT,
     room_number       INT,
-    slot_year         INT NOT NULL,
-    slot_term         VARCHAR(3) NOT NULL
     FOREIGN KEY       (building_name) references buildings(building_name),
     FOREIGN KEY       (subject_code, class_number) references classes(subject_code, class_number),
-    FOREIGN KEY       (tutor_id) references tutors(tutor_id),
     PRIMARY KEY       (slot_id)
 );
 CREATE TABLE times
 (
     time_id           INT unsigned NOT NULL AUTO_INCREMENT,
-    time_start_time   VARCHAR(32) NOT NULL,
-    time_end_time     VARCHAR(32) NOT NULL,
+    time_start        VARCHAR(32) NOT NULL,
+    time              VARCHAR(32) NOT NULL,
     day_name          VARCHAR(8) NOT NULL,
     FOREIGN KEY       (day_name) references days(day_name) ON DELETE RESTRICT,
     PRIMARY KEY       (time_id)
 );
 
 
-CREATE TABLE schedules
+CREATE TABLE slot_times
 (
     slot_id         INT unsigned NOT NULL,
     time_id         INT unsigned NOT NULL,
@@ -75,7 +71,7 @@ CREATE TABLE tutors(
     tutor_email         VARCHAR(80) NOT NULL,
 );
 
-CREATE TABLE tutor_course_agreements(
+CREATE TABLE tutor_agreed_classes(
     tutor_id        INT NOT NULL,
     subject_code    INT NOT NULL,
     class_number    INT NOT NULL,
@@ -84,12 +80,12 @@ CREATE TABLE tutor_course_agreements(
     FOREIGN KEY     (subject_code,class_number) REFERENCES classes(subject_code,class_number)
 );
 
-CREATE TABLE tutor_availibility(
+CREATE TABLE tutor_availibilities(
     tutor_id        INT NOT NULL,
     time_id        INT NOT NULL,
     PRIMARY KEY     (tutor_id, time_id),
     FOREIGN KEY     (tutor_id) REFERENCES tutors(tutor_id),
-    FOREIGN KEY     (time_id) REFERENCES times(time_id)
+    FOREIGN KEY     (time_id) REFERENCES times(time_id) ON DELETE RESTRICT
 );
 
 CREATE TABLE tutor_qualified_subjects(
@@ -97,7 +93,37 @@ CREATE TABLE tutor_qualified_subjects(
     subject_code    CHAR(3) NOT NULL,
     PRIMARY KEY     (tutor_id, subject_code),
     FOREIGN KEY     (tutor_id) REFERENCES tutors,
-    FOREIGN KEY     (subject_code) REFERENCES subjects(subject_code)
+    FOREIGN KEY     (subject_code) REFERENCES subjects(subject_code) ON DELETE RESTRICT
+);
+
+CREATE TABLE terms(
+    term_code       char(2) NOT NULL,
+    PRIMARY KEY     (term_code)
+);
+
+CREATE TABLE year_terms(
+    term_id         INT NOT NULL,
+    term_code       CHAR(3) NOT NULL,
+    term_year       INT NOT NULL,
+    PRIMARY KEY     (term_id),
+    FOREIGN KEY     (term_code) REFERENCES terms ON DELETE RESTRICT
+);
+
+
+CREATE TABLE slot_terms(
+    slot_id         INT NOT NULL,
+    term_id         CHAR(6) NOT NULL,
+    PRIMARY KEY     (slot_id, term_id),
+    FOREIGN KEY     (slot_id) REFERENCES slots,
+    FOREIGN KEY     (term_id) REFERENCES year_terms(term_id) ON DELETE RESTRICT
+);
+
+CREATE TABLE slot_tutors(
+    slot_id         INT NOT NULL,
+    tutor_id        INT NOT NULL,
+    PRIMARY KEY     (tutor_id, slot_id),
+    FOREIGN KEY     (tutor_id) REFERENCES tutors,
+    FOREIGN KEY     (slot_id) REFERENCES slots(slot_id)
 );
 
 
