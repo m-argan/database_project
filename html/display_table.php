d05874ac1469843b420a16de12832ad1dcaf5f35
Jenna Nicodemus <jenna.nicodemus@centre.edu>
2025-10-22 21:57:56 +0000

    Working PHP page

---
+++ html/display_table.php
@@ -0,0 +1,83 @@
+<?php
+    // For error checking
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    
+    // Config
+    $config = parse_ini_file('/home/jenna.nicodemus/mysql.ini');       // NOTE: NEED TO CHANGE
+    $dbname = 'clc_tutoring';
+    $conn = new mysqli(
+        $config['mysqli.default_host'],
+        $config['mysqli.default_user'],
+        $config['mysqli.default_pw'],
+        $dbname);
+    // Check errors in connection
+     if ($conn->connect_errno) {
+        echo "Error: Failed to make a MySQL connection, here is why: ". "<br>";
+        echo "Errno: " . $conn->connect_errno . "\n";
+        echo "Error: " . $conn->connect_error . "\n";
+        exit; // Quit this PHP script if the connection fails
+    }
+
+
+    // Formatting function
+    function format_result_as_table(mysqli_result $result): void {  ?>
+        <table style="width:100%">
+            <thead>
+                <tr>
+                    <?php
+                        // Header rows
+                        while ($field = $result->fetch_field()) {
+                            echo "<td><b>$field->name</b></td>";
+                        }
+                    ?>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    // Data rows
+                    while ($row = $result->fetch_row()) {
+                        echo "<tr>";
+                        for ($i = 0; $i < count($row); $i++) {
+                        echo "<td>$row[$i]</td>";
+                        }
+                        echo "</tr>";
+                    }
+            ?>
+            </tbody>
+        </table>
+    <?php 
+    }
+
+
+    // Whitelist possibilities for valid tables -- I'm sure there's a better way to check this...
+    $result = $conn->query("SHOW TABLES");
+    $is_a_table = false;
+    while ($tablename = $result->fetch_array()) {
+        if ($tablename[0] === htmlspecialchars( $_GET['tablename'] )) {
+            $is_a_table = true;
+            break;
+        }
+    }
+    if ($is_a_table === false) {
+        echo 'No table of that name found.';
+        exit();
+    }
+
+
+    // Otherwise, prepare statement to display table
+    $query = "SELECT * FROM " . htmlspecialchars( $_GET['tablename'] ) . ";";
+    $query = $conn->prepare($query);
+    $query->execute();
+    $result = $query->get_result();
+    
+
+    // Format webpage
+    format_result_as_table($result);    
+
+    
+    // Close connection
+    $conn->close();
+?>
