9f1fe5a35490b4143ff2f37879ddd68706666374
Madeleine Arganbright <m.arganbright@centre.edu>
2025-11-08 03:04:01 +0000

    alter works with dummy data

--- html/src/alter_tools.php
+++ html/src/alter_tools.php
@@ -1,28 +1,30 @@
 <?php
+include_once "display_table_tools.php";
  function display_alter_forms($conn) 
  { 
     $result = prepare_display_table($conn);
     ?>
-    <form action="alter_tools.php" method="POST">
-        <?php
+    <form method="POST">
+    <?php
     $row = $result->fetch_row();
     $i = 0;
 
     while ($field = $result->fetch_field()) {
-        echo $field->name;?>: <?php
-        ?> <input type="text" name="value" value = "<?php echo $row[$i]?>"/> <?php
+        echo $field->name . ": ";
+    ?>
+        <input type="text" name="value<?php echo $i; ?>" value="<?php echo $row[$i]; ?>"/>
+    <?php
         $i++;
     }
     ?>
-    <form action="display_alter.php" method="GET">
-    <p><input type="submit" value="Submit"/></p>
-        </form> 
+    <p><input type="submit" name="alter_btn" value="Submit"/></p>
+    </form>
+
     <?php
 }
     
 function alter_db($conn)
 {
-    echo "hello";
     $setElem = 'tutor_first_name';
     $IdElem = 'tutor_id';
     $IdValue = '380932';
@@ -30,22 +32,20 @@ function alter_db($conn)
     include_once "setup_tools.php";
     error_checking();
     $sel_tbl = "UPDATE ".htmlspecialchars($_GET['tablename'] )." SET ".$setElem." = ? WHERE ".$IdElem." = ".$IdValue.";";
-    // echo $sel_tbl;
     $up_stmt = $conn->prepare($sel_tbl);
     $up_stmt->bind_param("s",$setValue);
     $up_stmt->execute();
 }
 
-// function alter_db($conn, $table, $setElem, $setValue, $IdElem, $IdValue)
-// {
-//     include_once "setup_tools.php";
-//     error_checking();
-//     $sel_tbl = "UPDATE ".$table." SET ".$setElem." = ? WHERE ".$IdElem." = ".$IdValue.";";
-//     #echo $sel_tbl;
-//     $up_stmt = $conn->prepare($sel_tbl);
-//     $up_stmt->bind_param("s",$setValue);
-//     $up_stmt->execute();
-// }
-    
+function alt($conn)
+{
+    display_alter_forms($conn);
+
+    if (array_key_exists('alter_btn', $_POST)) {
+        alter_db($conn);
+        header("Location: {$_SERVER['REQUEST_URI']}", true, 303);
+        exit();
+    }
+}
     
 ?>
--- html/src/display_alter.php
+++ html/src/display_alter.php
@@ -7,8 +7,9 @@ include_once "display_table_tools.php";
 include_once "alter_tools.php";
 error_checking();
 $conn = config();
-display_alter_forms($conn);
-alter_db($conn);
+// display_alter_forms($conn);
+// alter_db($conn);
+alt($conn);
 $conn->close();
 
  ?>
