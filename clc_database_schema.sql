261c3de3cf817a035033c41fce706214ddb54555
hannah.morrison <hannah.morrison@centre.edu>
2025-11-11 19:53:03 +0000

    Started changing sql stuff, need to add places table

--- clc_database_schema.sql
+++ clc_database_schema.sql
@@ -32,35 +32,30 @@ CREATE TABLE slots
 (
     deleted_when      TIMESTAMP DEFAULT 0,
     slot_id           INT unsigned NOT NULL AUTO_INCREMENT,
+    time_block_id     INT unsigned NOT NULL,
     building_name     VARCHAR(7) DEFAULT "NA", /* Default value */
     subject_code      CHAR(3) NOT NULL,
     class_number      INT unsigned NOT NULL,
     room_number       INT,
+    tutor_id          INT unsigned NOT NULL,
     FOREIGN KEY       (building_name) references buildings(building_name) ON DELETE SET DEFAULT, 
     FOREIGN KEY       (subject_code, class_number) references classes(subject_code, class_number), /* ON DELETE TRIGGER DENY */
+    FOREIGN KEY       (time_block_id) references time_blocks(time_block_id),
+    FOREIGN KEY       (tutor_id) references tutors(tutor_id),
     PRIMARY KEY       (slot_id),
     CONSTRAINT correct_room_num CHECK (room_number BETWEEN 0 AND 600)
 );
 
 CREATE TABLE time_blocks
 (
-    time_id           INT unsigned NOT NULL AUTO_INCREMENT,
-    time_start        VARCHAR(32) NOT NULL,
-    time_end          VARCHAR(32) NOT NULL,
+    time_block_id           INT unsigned NOT NULL AUTO_INCREMENT,
+    time_block_start        VARCHAR(32) NOT NULL,
+    time_block_end          VARCHAR(32) NOT NULL,
     week_day_name     VARCHAR(8) NOT NULL,
     FOREIGN KEY       (week_day_name) references week_days(week_day_name) ON DELETE RESTRICT,
     PRIMARY KEY       (time_id)
 );
 
-
-CREATE TABLE slot_times
-(
-    slot_id         INT unsigned NOT NULL,
-    time_id         INT unsigned NOT NULL,
-    FOREIGN KEY     (slot_id) references slots(slot_id), /* ON DELETE TRIGGER DENY */
-    FOREIGN KEY     (time_id) references time_blocks(time_id) ON DELETE RESTRICT,
-    PRIMARY KEY     (slot_id, time_id)
-);
 
 CREATE TABLE tutors(
     deleted_when        TIMESTAMP DEFAULT 0,
@@ -94,29 +89,12 @@ CREATE TABLE terms(
 );
 
 CREATE TABLE year_terms(
-    term_id         INT NOT NULL,
-    term_code       CHAR(2) NOT NULL, 
-    term_year       INT NOT NULL,
-    PRIMARY KEY     (term_id),
+    year_term_code       CHAR(2) NOT NULL, 
+    year_term_year       INT NOT NULL,
+    PRIMARY KEY     (year_term_code, year_term_year),
     FOREIGN KEY     (term_code) REFERENCES terms(term_code) ON DELETE RESTRICT
 );
 
-
-CREATE TABLE slot_terms(
-    slot_id         INT unsigned NOT NULL,
-    term_id         INT NOT NULL,
-    PRIMARY KEY     (slot_id, term_id),
-    FOREIGN KEY     (slot_id) REFERENCES slots(slot_id), /* ON DELETE TRIGGER DENY */
-    FOREIGN KEY     (term_id) REFERENCES year_terms(term_id) ON DELETE RESTRICT
-);
-
-CREATE TABLE slot_tutors(
-    slot_id         INT unsigned NOT NULL,
-    tutor_id        INT NOT NULL,
-    PRIMARY KEY     (tutor_id, slot_id),
-    FOREIGN KEY     (tutor_id) REFERENCES tutors(tutor_id), /* ON DELETE TRIGGER DENY */
-    FOREIGN KEY     (slot_id) REFERENCES slots(slot_id)     /* ON DELETE TRIGGER DENY */
-);
 
 CREATE TABLE tutor_availibilities(
     tutor_id        INT NOT NULL,
