6c245c183c92658d1a275fecb2f90a084892188e
sgreen154 <stella.green@centre.edu>
2025-11-12 11:19:30 -0500

    Update full_schedule_view.php

--- html/src/full_schedule_view.php
+++ html/src/full_schedule_view.php
@@ -1,15 +1,10 @@
-<!-- Incomplete - encountering issues with display_table_tools.php. Stored procedure should work otherwise -->
 <!-- Database or PHP needs a way to tell the current semester for this page to work, not yet implemented -->
 
-<h2>Select a Subject:</h2>
+<h2>Select a Subject or Class:</h2>
         <form action="full_schedule_view.php" method="GET">
                 <p>Subject code(e.g. HIS, MAT): <input type="text" name="subject" /></p>
-        </form>
-<h2>Select a Class:</h2>
-        <form action="display_table.php" method="GET">
                 <p>Class (e.g. 110, 330): <input type="integer" name="class" /></p>
                 <p><input type="submit" value="See Details"/></p>
-				<input type="hidden" name="tablename" value="<?php echo htmlspecialchars($_GET['tablename']); ?>">
         </form>
 
 <?php
@@ -20,32 +15,31 @@
 
         if (isset($_GET['subject']))
         {
-                $subject = $_GET['subject'];
-		$allsubjects = 0;
-		if (isset($_GET['class']))
-		{
-			$class = (int)$_GET['class'];
-			$allclasses = 0;
-		}
-		else
-		{
-			$class = 0;
-			$allclasses = 1;
-		}
-	}
+                $subject = htmlspecialchars($_GET['subject']);
+                $allsubjects = 0;
+                if (isset($_GET['class']))
+                {
+                        $class = (int)htmlspecialchars($_GET['class']);
+                        $allclasses = 0;
+                }
+                else
+                {
+                        $class = 0;
+                        $allclasses = 1;
+                }
+        }
         else
         {
-		$subject = '';
-		$class = 0;
-		$allsubjects = 1;
-		$allclasses = 1;
-	}
-	$semester = 'Fa';
-	$year = 2025;
-        $query = "CALL full_schedule_view('$subject', '$class', '$semester', '$year', '$allsubjects', '$allclasses')";
+                $subject = '';
+                $class = 0;
+                $allsubjects = 1;
+                $allclasses = 1;
+        }
+        $semester = 'Fa';
+        $year = 2025;
+        $query = "CALL tutor_schedule_view(false, true, NULL, NULL, '$subject', '$class', '$semester', '$year', true, '$allsubjects', '$allclasses', false)";
         $result = $conn->query($query);
         format_result_as_table($result);
-        //Note: this does not handle wrong input
 
         $conn->close();
 ?>
