73c3ae7de92c02362c2948b16157eb33ddbff3ac
sgreen154 <stella.green@centre.edu>
2025-11-12 11:17:53 -0500

    Updated view procedures for new DB structure

--- clc_database_tools.sql
+++ clc_database_tools.sql
@@ -84,146 +84,145 @@ END;
 //
 DELIMITER ;
 
---Creates procedure which shows tutor name, id, email, and what classes they taught
---May need to remove semester column in future
+--Creates procedure which shows tutor name, id, email, what classes they taught, and which semester they taught it    
 DROP PROCEDURE IF EXISTS tutor_history_view;
 
 DELIMITER //
-CREATE PROCEDURE tutor_history_view(
+CREATE PROCEDURE tutor_history_view
+(
     IN first_name VARCHAR(30),
     IN last_name VARCHAR(30),
     IN all_students BOOLEAN
 )
 BEGIN
     SET @query =
-    'SELECT CONCAT(tutor_first_name, " ", tutor_last_name) AS `Name`,
+    'SELECT DISTINCT CONCAT(tutor_first_name, " ", tutor_last_name) AS `Name`,
     tutors.tutor_id AS `Student ID`,
     tutor_email AS `Email`,
-    GROUP_CONCAT(DISTINCT CONCAT(year_terms.term_code, " ", year_terms.term_year) SEPARATOR "," ) AS `Semesters Taught`,
-    GROUP_CONCAT(DISTINCT CONCAT(slots.subject_code, " ", slots.class_number) SEPARATOR "," ) AS `Classes Taught`
+    CONCAT(slots.subject_code, " ", slots.class_number) AS `Classes Taught`,
+    CONCAT(term_code, " ", year_term_year) AS `Semester Taught`
     FROM tutors
-    INNER JOIN slot_tutors ON (slot_tutors.tutor_id = tutors.tutor_id)
-    INNER JOIN slots ON (slots.slot_id = slot_tutors.slot_id)
-    INNER JOIN slot_terms on (slot_terms.slot_id = slots.slot_id)
-    INNER JOIN year_terms ON (year_terms.term_id = slot_terms.term_id)
-    INNER JOIN tutor_agreed_classes ON (tutor_agreed_classes.tutor_id = tutors.tutor_id)';
+    INNER JOIN slots ON (tutors.tutor_id = slots.tutor_id)
+    INNER JOIN time_blocks ON (time_blocks.time_block_id = slots.time_block_id)';
     IF all_students = false THEN
-        SET @query = CONCAT(@query, ' WHERE tutors.tutor_first_name = ? AND tutors.tutor_last_name = ?');
+        SET @query = CONCAT(@query, ' WHERE tutors.tutor_first_name = ? AND tutors.tutor_last_name = ?');     
     END IF;
-    SET @query = CONCAT(@query, ' GROUP BY tutors.tutor_id;');
-
+    SET @query = CONCAT(@query, ';');
     PREPARE stmt from @query;
     IF all_students = FALSE THEN
-        EXECUTE stmt USING first_name, last_name;
-ELSE
-        EXECUTE stmt;
-END IF;
+        EXECUTE stmt USING first_name, last_name;  
+    ELSE
+            EXECUTE stmt;
+    END IF;
     DEALLOCATE PREPARE stmt;
 END //
 
 DELIMITER ;
 
-
---Creates a view which shows tutor name, the class(es) they teach, and each time they teach it
---Sorting can be done by tutors, subjects, or both
+--Creates a view which shows tutor name, room, the class(es) taught, the times they were taught, and the semester they were taught
+--Sorting can be done by tutor, subject, class, or semester
+--View updated to work both for tutor_schedule (Chisley) and full_schedule (students)
+--Chisley view shows student name, but not room, student view shows room, but not tutor (all done via php pages)
 DROP PROCEDURE IF EXISTS tutor_schedule_view;
 
 DELIMITER //
 CREATE PROCEDURE tutor_schedule_view(
+        IN show_students BOOLEAN,
+        IN show_rooms BOOLEAN,
         IN first_name VARCHAR(30),
         IN last_name VARCHAR(30),
         IN subject_code CHAR(3),
+        IN class_number INT,
+        IN term VARCHAR(2),
+        IN t_year INT,
         IN all_students BOOLEAN,
-        IN all_subjects BOOLEAN
+        IN all_subjects BOOLEAN,
+        IN all_classes BOOLEAN,
+        IN all_terms BOOLEAN
 )
 BEGIN
-        SET @query =
-        'SELECT CONCAT(tutor_first_name, " ", tutor_last_name) AS `Name`,
+        SET @query = 'SELECT ';
+        IF show_students = true THEN
+                SET @query = CONCAT(@query, 'CONCAT(tutor_first_name, " ", tutor_last_name) AS `Name`,');
+        END IF;
+        IF show_rooms = true THEN
+                SET @query = CONCAT (@query, 'CONCAT(building_name, " ", place_room_number) AS `Location`,');
+        END IF;
+        SET @query = CONCAT(@query, '
         CONCAT(slots.subject_code, " ", slots.class_number) AS `Class`,
-        CONCAT(time_blocks.time_start, " - ", time_blocks.time_end, " ", time_blocks.week_day_name) AS `Time Offered`
+        CONCAT(time_block_start, " - ", time_block_end, " ", time_blocks.week_day_name) AS `Time Tutored`,
+        CONCAT(term_code, " ", year_term_year) AS `Semester Taught`
         FROM tutors
-        INNER JOIN slot_tutors ON (tutors.tutor_id = slot_tutors.tutor_id)
-        INNER JOIN slots ON (slots.slot_id = slot_tutors.slot_id)
-        INNER JOIN slot_times ON (slots.slot_id = slot_times.slot_id)
-        INNER JOIN time_blocks ON (time_blocks.time_id = slot_times.slot_id)
-        ';
+        INNER JOIN slots ON (tutors.tutor_id = slots.tutor_id)
+        INNER JOIN time_blocks ON (time_blocks.time_block_id = slots.time_block_id)
+        ');
 
         SET @w = '';
-
+        SET @need_and = false;
+        IF all_students = false OR all_subjects = false  OR all_terms = false THEN
+                SET @w = 'WHERE ';
+        END IF;
         IF all_students = false THEN
-                SET @w = ' WHERE tutors.tutor_first_name = ? AND tutors.tutor_last_name = ?';
-                IF all_subjects = false THEN
-                        SET @w = CONCAT(@w, ' AND slots.subject_code = ?');
+                SET @w = CONCAT(@w, ' tutors.tutor_first_name = ? AND tutors.tutor_last_name = ? ');
+                SET @need_and = true;
+        END IF;
+        IF all_subjects = false THEN
+                IF @need_and = true THEN
+                        SET @w = CONCAT(@w, ' AND ');      
                 END IF;
-        ELSEIF all_subjects = false THEN
-                SET @w = ' WHERE slots.subject_code = ?';
+                SET @w = CONCAT(@w, ' slots.subject_code = ? ');
+                SET @need_and = true;
+        END IF;
+        IF all_classes = false THEN
+                IF @need_and = true THEN
+                        SET @w = CONCAT(@w, ' AND ');      
+                END IF;
+                SET @w = CONCAT(@w, ' slots.class_number = ?');
+                SET @need_and = true;
+        END IF;
+        IF all_terms = false THEN
+                IF @need_and = true THEN
+                        SET @w = CONCAT(@w, ' AND ');      
+                END IF;
+                SET @w = CONCAT(@w, 'term_code = ? AND year_term_year = ?');
         END IF;
 
-        SET @query = CONCAT(@query, @w);
+        SET @query = CONCAT(@query, @w, ';');
 
         PREPARE stmt FROM @query;
-        IF all_students = false AND all_students = false THEN
-                EXECUTE stmt USING first_name, last_name, subject_code;
+
+        IF all_students = false AND all_subjects = false AND all_classes = false AND all_terms = false THEN
+            EXECUTE stmt USING first_name, last_name, subject_code, class_number, term, t_year;
+        ELSEIF all_students = false AND all_subjects = false AND all_classes = false THEN
+            EXECUTE stmt USING first_name, last_name, subject_code, class_number;
+        ELSEIF all_students = false AND all_subjects = false AND all_terms = false THEN
+            EXECUTE stmt USING first_name, last_name, subject_code, term, t_year;
+        ELSEIF all_students = false AND all_subjects = false THEN
+            EXECUTE stmt USING first_name, last_name, subject_code;
+        ELSEIF all_students = false AND all_terms = false THEN
+            EXECUTE stmt USING first_name, last_name, term, t_year;
+        ELSEIF all_subjects = false AND all_classes = false AND all_terms = false THEN
+            EXECUTE stmt USING subject_code, class_number, term, t_year;
+        ELSEIF all_subjects = false AND all_classes = false THEN
+            EXECUTE stmt USING subject_code, class_number; 
+        ELSEIF all_subjects = false AND all_terms = false THEN
+            EXECUTE stmt USING subject_code, term, t_year; 
         ELSEIF all_students = false THEN
-                EXECUTE stmt USING first_name, last_name;
+            EXECUTE stmt USING first_name, last_name;      
         ELSEIF all_subjects = false THEN
-                EXECUTE stmt USING subject_code;
+            EXECUTE stmt USING subject_code;
+        ELSEIF all_terms = false THEN
+            EXECUTE stmt USING term, t_year;
         ELSE
-                EXECUTE stmt;
+            EXECUTE stmt;
         END IF;
+
         DEALLOCATE PREPARE stmt;
 END //
 
 DELIMITER ;
 
-
---Creates a view which shows room building and number, tutoring time and day, class subject and number
---Sorting done by subject or class
---Procedure also takes semester as input, but this should not be selectable by students (use current semester)
-DROP PROCEDURE IF EXISTS full_schedule_view;
-
-DELIMITER //
-CREATE PROCEDURE full_schedule_view(
-    IN subject_code CHAR(3),
-    IN class_number INT,
-    IN semester CHAR(2),
-    IN s_year INT,
-    IN all_subjects BOOLEAN,
-    IN all_classes BOOLEAN
-)
-BEGIN
-    SET @query =
-    'SELECT CONCAT(slots.subject_code, " ", slots.class_number) AS `Class`,
-    CONCAT(slots.building_name, " ", slots.room_number) AS `Room`,
-    CONCAT(time_blocks.time_start, " ", time_blocks.time_end, " ", time_blocks.week_day_name) AS `Time Offered`
-    FROM slots
-    INNER JOIN slot_times ON (slots.slot_id = slot_times.slot_id)
-    INNER JOIN time_blocks ON (time_blocks.time_id = slot_times.slot_id)
-    ';
-    IF all_subjects = false THEN
-        SET @w = CONCAT(@w, 'AND slots.subject_code = ?');
-        IF all_classes = false THEN
-            SET @w = CONCAT(@w, 'AND slots.class_number = ?');
-        END IF;
-    END IF;
-
-    SET @query = CONCAT(@query, @w, ';');
-
-    PREPARE stmt FROM @query;
-
-    IF all_subjects = false AND all_classes = false THEN
-        EXECUTE stmt USING semester, s_year, subject_code, class_number;
-    ELSEIF all_subjects = false AND all_classes = true THEN
-        EXECUTE stmt USING semester, s_year, subject_code;
-    ELSE
-        EXECUTE stmt USING semester, s_year;
-    END IF;
-END //
-
-DELIMITER ;
-
 --Grant permissions
-GRANT EXECUTE ON PROCEDURE full_schedule_view TO 'webuser'@'localhost';
 GRANT EXECUTE ON PROCEDURE tutor_schedule_view TO 'webuser'@'localhost';
 GRANT EXECUTE ON PROCEDURE tutor_history_view TO 'webuser'@'localhost';
 GRANT EXECUTE ON PROCEDURE clc_tutoring.deny_classes TO 'webuser'@'localhost';
