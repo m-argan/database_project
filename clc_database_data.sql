b77c27fb2339c09591fb2bc50c5afc8332877eaf
sgreen154 <stella.green@centre.edu>
2025-11-12 11:12:01 -0500

    Added data to test views

--- clc_database_data.sql
+++ clc_database_data.sql
@@ -46,22 +46,44 @@ INSERT INTO places(building_name, place_room_number) VALUES
 ("Grant", 101);
 
 INSERT INTO tutors(tutor_id, tutor_first_name, tutor_last_name, tutor_email) VALUES
-(380932, "Hannah", "Morrison", "hannah.morrison@centre.edu");
+(380932, "Hannah", "Morrison", "hannah.morrison@centre.edu"),
+(380012, "Stella", "Green", "stella.green@centre.edu"),    
+(000001, "William", "Bailey", "william.bailey@centre.edu");
 
 INSERT INTO time_blocks(time_block_start, time_block_end, week_day_name,term_code,year_term_year) VALUES
-("7:00", "8:00", "Su", "FA", 2025);
+("7:00", "8:00", "Su", "FA", 2025),
+("7:00", "8:00", "M", "FA", 2025),
+("7:30", "8:30", "Su", "SP", 2025),
+("7:30", "8:30", "Su", "SP", 2024);
 
 INSERT INTO tutor_agreed_classes(tutor_id,subject_code, class_number) VALUES
-(380932, "MAT", 330);
+(380932, "MAT", 330),
+(380012, "CSC", 332),
+(000001, "ARS", 250),
+(000001, "ENS", 110);
 
 INSERT INTO tutor_qualified_subjects(tutor_id, subject_code) VALUES
-(380932, "MAT");
+(380932, "MAT"),
+(380012, "CSC"),
+(000001, "ARS"),
+(000001, "ENS");
 
 INSERT INTO tutor_availibilities(tutor_id, time_block_id) VALUES
-(380932,1);
+(380932,1),
+(380932,2),
+(380932,3),
+(380932,4),
+(380012,1),
+(000001,1),
+(000001,2);
 
 INSERT INTO slots(time_block_id, building_name, place_room_number,subject_code,class_number,tutor_id) VALUES
-(1,"Crounse", 111,"MAT", "330", 380932);
+(1,"Crounse", 111,"MAT", "330", 380932),
+(2,"Crounse", 111,"MAT", "330", 380932),
+(3,"Crounse", 111,"MAT", "330", 380932),
+(1,"Olin", 111,"CSC", "332", 380012),
+(1,"Grant", 101,"ARS", "250", 000001),
+(2,"Young", 111,"ENS", "110", 000001);
 
 
 
