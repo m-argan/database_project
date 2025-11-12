8679131296ceb578564d17b8ffdc33ab5ae70bef
sgreen154 <stella.green@centre.edu>
2025-11-12 11:20:28 -0500

    Update student_history_view.php

--- html/src/student_history_view.php
+++ html/src/student_history_view.php
@@ -1,36 +1,32 @@
-<!-- Incomplete - encountering issues with display_table_tools.php. Stored procedure should work otherwise -->
-
 <h2>Select a Student:</h2>
         <form action="student_history_view.php" method="GET">
                 <p>First name: <input type="text" name="firstname" /></p>
                 <p>Last name: <input type="text" name="lastname" /></p>
                 <p><input type="submit" value="See Details"/></p>
-				<input type="hidden" name="tablename" value="<?php echo htmlspecialchars($_GET['tablename']); ?>">
-	</form>
+        </form>
 
 <?php
-	include_once "setup_tools.php";
-	include_once "display_table_tools.php";
-	error_checking();
-	$conn = config();
+        include_once "setup_tools.php";
+        include_once "display_table_tools.php";
+        error_checking();
+        $conn = config();
 
-	if (isset($_GET['firstname']) && isset($_GET['lastname'])) 
-	{
-		$first = $_GET['firstname'];
-		$last = $_GET['lastname'];
-		$allstudents = 0;
-	}
-	else
-	{
-		$first = '';
-		$last = '';
-		$allstudents = 1;
-	}
-	$query = "CALL tutor_history_view('$first', '$last', '$allstudents')";
-	$result = $conn->query($query);
-	format_result_as_table($result);
-	//Note: this does not handle wrong input
+        if (isset($_GET['firstname']) && isset($_GET['lastname']))
+        {
+                $first = htmlspecialchars($_GET['firstname']);
+                $last = htmlspecialchars($_GET['lastname']);
+                $allstudents = 0;
+        }
+        else
+        {
+                $first = NULL;
+                $last = NULL;
+                $allstudents = 1;
+        }
+        $query = "CALL tutor_history_view('$first', '$last', '$allstudents')";
+        $result = $conn->query($query);
+        format_result_as_table($result);
+        //Note: this does not handle wrong input
 
-	$conn->close();
+        $conn->close();
 ?>
-
