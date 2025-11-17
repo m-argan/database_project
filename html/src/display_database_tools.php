d1c378baf82b3e48fc4be24decd015101e6935f4
Madeleine Arganbright <m.arganbright@centre.edu>
2025-11-17 17:42:25 -0500

    .

--- html/src/display_database_tools.php
+++ html/src/display_database_tools.php
@@ -11,7 +11,7 @@
         while ($tablename = $result->fetch_array()) {
             echo "<li> $tablename[0] </li>";
             // echo "hi";
-            echo ?><a href=""><?php echo $tablename[0]?></a>;<?php
+            echo ?><a href=""><?php echo $tablename[0];?></a><?php
         }
         echo "</ul>";
 
