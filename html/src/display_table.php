8940045fac5c82da069bf949d62e6da941f5e4b1
Madeleine Arganbright <m.arganbright@centre.edu>
2025-11-19 20:44:06 -0500

    .

--- html/src/display_table.php
+++ html/src/display_table.php
@@ -3,14 +3,13 @@
     
     require_once __DIR__ . "/setup_tools.php";
     require_once __DIR__ . "/display_table_tools.php";
-    require_once __DIR__ . "/display_database_tools.php";
+    // require_once __DIR__ . "/display_database_tools.php";
     // include_once "alter_database.php";
 
     session_start();
 
     error_checking();
     $conn = config();
-    render_display_database_page($conn);
     render_display_table_page($conn);
     # this was just to test alter_db
     #alter_db($conn,"tutors","tutor_first_name", "Madeleine", "tutor_id", 380932);
