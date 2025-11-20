508cf9df06d9010d4d17a04de00afe137bb593d0
Madeleine Arganbright <m.arganbright@centre.edu>
2025-11-20 15:14:04 -0500

    .

--- html/src/display_database_tools.php
+++ html/src/display_database_tools.php
@@ -55,7 +55,7 @@
             <title>CLC Database</title>
         </head>
         <body>
-          <!-- <div class="header">
+          <div class="header">
 
             <div class="headertitle">
                 <h1>CLC Database</h1>
@@ -67,7 +67,7 @@
                 // display_form();
             ?></div>
             </div>
-             -->
+            
             <div class="sidenav"><?php
                 // List the tables of the database
                 list_tables($conn);
@@ -78,11 +78,11 @@
             </div>
         </body>
 
-        <!-- <footer>
+        <footer>
             <br>
             <p>CSC 362: Database Systems Fall 2025</p>
             <p>Developed by Hannah Morrison, Stella Green, Madeleine Arganbright, Jenna Nicodemus</p>
-        </footer> -->
+        </footer>
         </html>
    <?php }
 ?>
