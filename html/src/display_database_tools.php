d200091f8994426ea2d01ba42ac622a0b97fcfd5
Madeleine Arganbright <m.arganbright@centre.edu>
2025-11-20 14:14:23 -0500

    .

--- html/src/display_database_tools.php
+++ html/src/display_database_tools.php
@@ -1,104 +1,85 @@
-
-
 <?php
-   // Function definitions for display_database, AKA (currently) index.php.
+    // Function definitions for display_database, AKA (currently) index.php.
 
 
+    // Function for listing tables of database:
+    function list_tables($conn) {
+        $dblist = "SHOW TABLES";
+        $result = $conn->query($dblist);
+
+        echo "<ul>";
+        while ($tablename = $result->fetch_array()) {
+            //echo "<li> $tablename[0] </li>";
+           // $url = "http://34.61.211.5/tutor_db/src/display_table.php?tablename=".$tablename[0];
+           $url = "display_table.php?tablename=".$tablename[0];
+            // echo $url;
+            echo '<br><a href="'.$url.'">' . $tablename[0] . '</a><br>';
+
+        }
+        echo "</ul>";
+
+    }
+
+    // Function for displaying form at the bottom of the page:
+    function display_form() { ?>
+    <!-- <h2>View a table:</h2> -->
+    <!-- <form action="display_table.php" method="GET"> -->
+        <!-- <p>Table name: <input type="text" name="tablename" /></p> -->
+        <!-- <p><input type="submit" value="See Details"/></p> -->
+    <!-- </form> -->
+    <!-- ADDED BY HANNAH -->
+     <!-- <h2>Add to table:</h2>
+    <form action="display_adding.php" method="GET">
+        <p>Table name: <input type="text" name="tablename" /></p>
+        <p><input type="submit" value="See Details"/></p>
+    </form> -->
+    <!-- ADDED BY STELLA -->
+        <table style="border-style: none">
+                <tr style="border-style: none">
+                        <td style="border-style: none"><a href="student_history_view.php">View Student</a></td>
+                        <td style="border-style: none"><a href="student_schedule_view.php">View Student Schedule</a></td>
+                        <td style="border-style: none"><a href="full_schedule_view.php">View Full Schedule</a></td>
+                        <!-- <td style="border-style: none"></td> -->
+                </tr>
+        </table>
+    <?php
+    }
 
 
-   // Function for listing tables of database:
-   function list_tables($conn) {
-       $dblist = "SHOW TABLES";
-       $result = $conn->query($dblist);
+    // Function for rendering the page:
+    function render_display_database_page($conn) { ?>
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <link rel="stylesheet" href="nav.css">
+            <title>CLC Database</title>
+        </head>
+        <body>
+          <div class="header">
 
+            <div class="headertitle">
+                <h1>CLC Database</h1>
+            </div>
 
-       echo "<ul>";
-       while ($tablename = $result->fetch_array()) {
-           //echo "<li> $tablename[0] </li>";
-          // $url = "http://34.61.211.5/tutor_db/src/display_table.php?tablename=".$tablename[0];
-          $url = "display_table.php?tablename=".$tablename[0];
-           // echo $url;
-           echo '<br><a href="'.$url.'">' . $tablename[0] . '</a><br>';
+            <div class="topnav">
+            <?php
+                // displays the views
+                display_form();
+            ?></div>
+            </div>
 
+            <div class="sidenav"><?php
+                // List the tables of the database
+                list_tables($conn);
+            ?></div>
+        </body>
 
-       }
-       echo "</ul>";
-
-
-   }
-
-
-   // Function for displaying form at the bottom of the page:
-   function display_form() { ?>
-   <!-- <h2>View a table:</h2> -->
-   <!-- <form action="display_table.php" method="GET"> -->
-       <!-- <p>Table name: <input type="text" name="tablename" /></p> -->
-       <!-- <p><input type="submit" value="See Details"/></p> -->
-   <!-- </form> -->
-   <!-- ADDED BY HANNAH -->
-    <!-- <h2>Add to table:</h2>
-   <form action="display_adding.php" method="GET">
-       <p>Table name: <input type="text" name="tablename" /></p>
-       <p><input type="submit" value="See Details"/></p>
-   </form> -->
-   <!-- ADDED BY STELLA -->
-       <table style="border-style: none">
-               <tr style="border-style: none">
-                       <td style="border-style: none"><a href="student_history_view.php">View Student</a></td>
-                       <td style="border-style: none"><a href="student_schedule_view.php">View Student Schedule</a></td>
-                       <td style="border-style: none"><a href="full_schedule_view.php">View Full Schedule</a></td>
-                       <!-- <td style="border-style: none"></td> -->
-               </tr>
-       </table>
-   <?php
-   }
-
-
-
-
-   // Function for rendering the page:
-   function render_display_database_page($conn) { ?>
-       <!DOCTYPE html>
-       <html>
-       <head>
-           <link rel="stylesheet" href="nav.css">
-           <title>CLC Database</title>
-       </head>
-       <body>
-         <div class="header">
-
-
-           <div class="headertitle">
-               <h1>CLC Database</h1>
-           </div>
-
-
-           <div class="topnav">
-           <?php
-               // displays the views
-               display_form();
-           ?></div>
-           </div>
-
-
-           <div class="sidenav"><?php
-               // List the tables of the database
-               list_tables($conn);
-           ?></div>
-
-
-           <div class="main">
-           <h2>Welcome to the Tutoring Database!</h2>
-           </p>
-
-           <footer>
-           <br>
-           <p>CSC 362: Database Systems Fall 2025</p>
-           <p>Developed by Hannah Morrison, Stella Green, Madeleine Arganbright, Jenna Nicodemus</p>
-       </footer>
-       </body>
-       </html>
-  <?php }
+        <footer>
+            <br>
+            <p>CSC 362: Database Systems Fall 2025</p>
+            <p>Developed by Hannah Morrison, Stella Green, Madeleine Arganbright, Jenna Nicodemus</p>
+        </footer>
+        </html>
+   <?php }
 ?>
-
 
