f18a8f91dddd7a1e7ce00e3886bb1717df28c133
Madeleine Arganbright <m.arganbright@centre.edu>
2025-11-20 14:19:01 -0500

    .

--- html/src/display_database_tools.php
+++ html/src/display_database_tools.php
@@ -68,13 +68,13 @@
             ?></div>
             </div>
         <div class="main_content">
+            <div class="main">
+                <h1>Welcome to the Tutoring Database!</h1>
+            </div>
             <div class="sidenav"><?php
                 // List the tables of the database
                 list_tables($conn);
-            ?></div>
-
-            <div class="main">
-                <h1>Welcome to the Tutoring Database!</h1>
+            ?>
             </div>
         </div>
         </body>
