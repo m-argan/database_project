47a996f7202d39e6ff13c59733a3ef0785c9315e
Madeleine Arganbright <m.arganbright@centre.edu>
2025-11-20 15:26:04 -0500

    .

--- html/src/display_database_tools.php
+++ html/src/display_database_tools.php
@@ -78,11 +78,11 @@
             </div>
         </body>
 
-        <footer>
+        <!-- <footer>
             <br>
             <p>CSC 362: Database Systems Fall 2025</p>
             <p>Developed by Hannah Morrison, Stella Green, Madeleine Arganbright, Jenna Nicodemus</p>
-        </footer>
+        </footer> -->
         </html>
    <?php }
 ?>
