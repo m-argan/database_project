930599b5ed272ab0355ccbca26d97d7d18292395
Madeleine Arganbright <m.arganbright@centre.edu>
2025-12-04 16:57:59 -0500

    .

--- html/src/calendar_view.php
+++ html/src/calendar_view.php
@@ -14,50 +14,46 @@ start_view_capture();
         require_once __DIR__ . "/display_table_tools.php";
         error_checking();
         $conn = config();
-        gg();
-        if(isset($_POST['submit']))
+
+        
+        if (isset($_GET['subject']) && !empty($_GET['subject']))
         {
-                gg();
-        }
-        function gg(){
-                if (isset($_GET['subject']) && !empty($_GET['subject']))
+                $subject = htmlspecialchars($_GET['subject']);
+                $allsubjects = 0;
+                if (isset($_GET['class']) && !empty($_GET['class']))
                 {
-                        $subject = htmlspecialchars($_GET['subject']);
-                        $allsubjects = 0;
-                        if (isset($_GET['class']) && !empty($_GET['class']))
-                        {
-                                $class = (int)htmlspecialchars($_GET['class']);
-                                $allclasses = 0;
-                        }
-                        else
-                        {
-                                $class = 0;
-                                $allclasses = 1;
-                        }
+                        $class = (int)htmlspecialchars($_GET['class']);
+                        $allclasses = 0;
                 }
                 else
                 {
-                        $subject = '';
                         $class = 0;
-                        $allsubjects = 1;
                         $allclasses = 1;
                 }
-                $query = "CALL calendar_pivot_view('$subject', '$class', '$allsubjects', '$allclasses')";
-                $result = $conn->query($query);
-                format_result_as_calendar($result);
-
-                if (isset($result) && $result instanceof mysqli_result) {
-                $result->free();
-                mysqli_next_result($conn);
-                }
-                
-                
-                if(isset($_POST["role_student"])){
-                finish_view_capture_and_render($conn, true, false);
-                }
-                else{
-                finish_view_capture_and_render($conn, false, false);
-                }
         }
+        else
+        {
+                $subject = '';
+                $class = 0;
+                $allsubjects = 1;
+                $allclasses = 1;
+        }
+        $query = "CALL calendar_pivot_view('$subject', '$class', '$allsubjects', '$allclasses')";
+        $result = $conn->query($query);
+        format_result_as_calendar($result);
+
+        if (isset($result) && $result instanceof mysqli_result) {
+            $result->free();
+            mysqli_next_result($conn);
+        }
+        
+        echo(var_dump($_POST));
+        if(isset($_POST["role_student"])){
+            finish_view_capture_and_render($conn, true, false);
+        }
+        else{
+            finish_view_capture_and_render($conn, false, false);
+        }
+
         $conn->close();
 ?>
