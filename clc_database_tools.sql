f063c0b16ed1c723992a10f003b896ae777c1d03
hannah.morrison <hannah.morrison@centre.edu>
2025-12-04 03:12:12 +0000

    changed permissions to clc_tutoring.calendar_pivot_view

--- clc_database_tools.sql
+++ clc_database_tools.sql
@@ -426,10 +426,10 @@ END //
 DELIMITER ;
 
 --Grant permissions
-GRANT EXECUTE ON PROCEDURE calendar_pivot_view TO 'webuser'@'localhost';
-GRANT EXECUTE ON PROCEDURE tutor_agreement_form_view TO 'webuser'@'localhost';
-GRANT EXECUTE ON PROCEDURE tutor_schedule_view TO 'webuser'@'localhost';
-GRANT EXECUTE ON PROCEDURE tutor_history_view TO 'webuser'@'localhost';
+GRANT EXECUTE ON PROCEDURE clc_tutoring.calendar_pivot_view TO 'webuser'@'localhost';
+GRANT EXECUTE ON PROCEDURE clc_tutoring.tutor_agreement_form_view TO 'webuser'@'localhost';
+GRANT EXECUTE ON PROCEDURE clc_tutoring.tutor_schedule_view TO 'webuser'@'localhost';
+GRANT EXECUTE ON PROCEDURE clc_tutoring.tutor_history_view TO 'webuser'@'localhost';
 GRANT EXECUTE ON PROCEDURE clc_tutoring.deny_classes TO 'webuser'@'localhost';
 GRANT EXECUTE ON PROCEDURE clc_tutoring.deny_slots TO 'webuser'@'localhost';
 GRANT EXECUTE ON PROCEDURE clc_tutoring.deny_tutors TO 'webuser'@'localhost';
