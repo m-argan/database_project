a24791dd47fbb7503232fef7bbed0a9d003f02c3
Madeleine Arganbright <m.arganbright@centre.edu>
2025-12-04 17:25:53 -0500

    .

--- html/src/calendar_view.php
+++ html/src/calendar_view.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/display_views_tools.php';
 start_view_capture();
 ?>
 <h2>Select a Subject or Class:</h2>
-        <form action="calendar_view.php" method="POST">
+        <form action="calendar_view.php" method="GET">
                 <p>Subject code(e.g. HIS, MAT): <input type="text" name="subject" /></p>
                 <p>Class (e.g. 110, 330): <input type="integer" name="class" /></p>
                 <p><input type="submit" value="See Details"/></p>
@@ -14,8 +14,7 @@ start_view_capture();
         require_once __DIR__ . "/display_table_tools.php";
         error_checking();
         $conn = config();
-        
-        
+
         if (isset($_GET['subject']) && !empty($_GET['subject']))
         {
                 $subject = htmlspecialchars($_GET['subject']);
@@ -42,14 +41,11 @@ start_view_capture();
         $result = $conn->query($query);
         format_result_as_calendar($result);
 
-        if (isset($result) && $result instanceof mysqli_result) {
+         if (isset($result) && $result instanceof mysqli_result) {
             $result->free();
             mysqli_next_result($conn);
         }
-        
-        // if(isset($_POST["role_student"])){
-        //     finish_view_capture_and_render($conn, true, false);
-        // }
+
         finish_view_capture_and_render($conn, true, false);
 
         $conn->close();
