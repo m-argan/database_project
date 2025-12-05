428e7551255a7941fae809cb07d761972d88d68c
Madeleine Arganbright <m.arganbright@centre.edu>
2025-12-04 22:48:00 -0500

    .

--- html/src/display_alter.php
+++ html/src/display_alter.php
@@ -7,12 +7,11 @@ require_once __DIR__ . "/alter_tools.php";
 error_checking();
 $conn = config();
 
-if (!empty($error_message)): ?>
-    <p style="color:red;"><?php echo $error_message; ?></p>
-<?php endif; 
-
 if (isset($_POST['submit_btn'])) {
     perform_alter($conn);
+    if (!empty($error_message)): ?>
+    <p style="color:red;"><?php echo $error_message; ?></p>
+    <?php endif; 
     //display_session_errors();
 } else {
     $result = get_result($conn);
