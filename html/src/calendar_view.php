b8d931d605c38f299891d57d8cf809c3df88529c
Madeleine Arganbright <m.arganbright@centre.edu>
2025-12-04 21:55:21 -0500

    .

--- html/src/calendar_view.php
+++ html/src/calendar_view.php
@@ -50,7 +50,7 @@ start_view_capture();
 
         finish_view_capture_and_render($conn, true, false);
 
-        echo "<a href='index.php'>Back to login</a>";
+        //echo "<a href='index.php'>Back to login</a>";
 
         $conn->close();
 ?>
