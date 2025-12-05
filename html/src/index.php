de0eac61ea93293a8c8501f3d887e629364da678
Madeleine Arganbright <m.arganbright@centre.edu>
2025-12-04 21:57:24 -0500

    revert

--- html/src/index.php
+++ html/src/index.php
@@ -8,37 +8,39 @@
     $password = file_get_contents('../../../password.txt');
     $password = trim($password);
     
-    echo(var_dump($_POST));
     // Set up and render page
     error_checking();
     $conn = config();
 
-    if(!isset($_POST['role_admin']) && !isset($_POST['role_student']))
-    {render_login($conn, False);}
-    // echo(var_dump($_GET));
-    elseif(isset($_POST['role_admin']))
+    // Function to check password against password in file saved to machine
+    function login($password, $conn){
+        // If password is correct, display Admin view
+        if($_POST['admin_password'] == $password)
         {
-            if($_POST['admin_password'] == $password)
-            {
-                render_homepage($conn);
-            }
-            elseif($_POST['admin_password'] != $password)
-            {
-                render_login($conn, True);
-            }
+            render_homepage($conn);
         }
-    elseif(isset($_POST['role_student']))
-    {
-        // if (session_status() === PHP_SESSION_NONE) {
-        //     session_start();
-        //     $_SESSION["role"] = "student";
-        // }
-    
-        include 'calendar_view.php'; 
-
+        elseif($_POST['admin_password'] != $password)
+        {
+            // Reload login page, but this time with an error message
+            render_login($conn, True);
+        }
     }
     
-    // Close connection
+    // If roles have not been set, default to login page
+    if(!isset($_POST['role_admin']) && !isset($_POST['role_student']))
+    {render_login($conn, False);}
+
+    elseif(isset($_POST['role_admin']))
+        {
+            login($password, $conn);
+        }
+
+    elseif(isset($_POST['role_student']))
+    {
+        include 'calendar_view.php'; 
+    }
+    
+    // Close connection -> commented out because calendary_view already closes the connection
 
     // try{
     //     $conn->close();
