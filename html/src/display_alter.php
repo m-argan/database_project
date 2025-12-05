accfb65a59debef7effb7d68abcdc7b9ffee61ca
Madeleine Arganbright <m.arganbright@centre.edu>
2025-12-04 22:51:14 -0500

    .

--- html/src/display_alter.php
+++ html/src/display_alter.php
@@ -8,17 +8,25 @@ error_checking();
 $conn = config();
 
 if (isset($_POST['submit_btn'])) {
-    perform_alter($conn);
-    if (!empty($error_message)): ?>
-    <p style="color:red;"><?php echo $error_message; ?></p>
-    <?php endif; 
-    //display_session_errors();
+
+    // Call perform_alter() and capture error message directly
+    $error_message = perform_alter($conn);
+
 } else {
-    $result = get_result($conn);
-    alt($conn);
+    $error_message = ""; // default if not submitted
 }
 
-$conn->close();
 ?>
 
+<?php
+if (!empty($error_message)): ?>
+    <p style="color:red;"><?= htmlspecialchars($error_message); ?></p>
+<?php endif; ?>
 
+
+<?php  
+// Now display the form or table normally:
+$result = get_result($conn);
+alt($conn);
+
+$conn->close();
